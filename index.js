5b7a386bafd6ff1b4c21fe603b3544bbb078d9e8
maxieb1992 <maxboelens@live.nl>
2024-06-22 14:57:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to EPIMAX: http://localhost:${port}`);
+  console.log(`🔗 Powered By EPIMAX`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["YOUTUBE","SPOTIFY","INSTAGRAM","TWITCH"];
 
 
 let currentIndex = 0;
