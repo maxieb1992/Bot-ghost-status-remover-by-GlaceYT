e558e57419f4f32495cac94a3a6f4fc0ec366151
maxieb1992 <maxboelens@live.nl>
2024-06-22 14:25:40 +0200

    Update2 index.js

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.get('/', (req, res) => {
 });
 app.listen(port, () => {
   console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Powered By EPIMAX`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["SPOTIFY","YOUTUBE","INSTAGRAM","TWITCH"];
 
 
 let currentIndex = 0;
